fe08eae6012442da96afbc02254e4f94db514024
Alexander Frick <alex@pop-os.localdomain>
2023-04-05 00:28:37 -0500

    1.0.2

--- app/app.js
+++ app/app.js
@@ -2,15 +2,23 @@ const { app, BrowserWindow } = require('electron')
 
 function createWindow () {
 	let mainWindow = new BrowserWindow({
-		width: 300,
-		height: 460,
-		icon:'app/icon.png',
+		width: 350,
+		height: 450,
+		icon:'app/assets/ic_calculator_64.png',
 		webPreferences: {
-		  nodeIntegration: true
+		  nodeIntegration: true,
+		  webviewTag: true
 		}
 	  });
 	mainWindow.loadFile('./app/index.html');
-	mainWindow.setMenuBarVisibility(false);
-	mainWindow.setResizable(false);
+	mainWindow.setMenuBarVisibility(true);
+	mainWindow.setResizable(true);
 }
 app.whenReady().then(createWindow);
+app.commandLine.appendSwitch('enable-experimental-web-platform-features');
+app.commandLine.appendSwitch('allow-file-access-from-files');
+app.commandLine.appendSwitch('enable-local-file-accesses');
+app.commandLine.appendSwitch('enable-quic');
+app.commandLine.appendSwitch('enable-ui-devtools');
+app.commandLine.appendSwitch('ignore-gpu-blocklist');
+app.commandLine.appendSwitch('enable-gpu-rasterization');
