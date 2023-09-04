0c785cd3b820269bd0ee8e17e6b1e3937d9cf9f8
Alexander Frick <Alex313031@gmail.com>
2023-09-04 11:25:12 -0500

    1.1.0

--- app/app.js
+++ app/app.js
@@ -7,6 +7,19 @@ const url = require('url');
 // Initialize Electron remote module
 require('@electron/remote/main').initialize();
 
+// Get app version from package.json
+var appVersion = app.getVersion();
+// Get Electron versions
+var electronVersion = process.versions.electron;
+var chromeVersion = process.versions.chrome;
+var nodeVersion = process.versions.node;
+var v8Version = process.versions.v8;
+
+// Globally export what OS we are on
+const isLinux = process.platform === 'linux';
+const isWin = process.platform === 'win32';
+const isMac = process.platform === 'darwin';
+
 function createWindow () {
     let mainWindow = new BrowserWindow({
       title: 'Electron Calculator',
@@ -31,6 +44,99 @@ function createWindow () {
       }
     });
     require("@electron/remote/main").enable(mainWindow.webContents);
+    Menu.setApplicationMenu(Menu.buildFromTemplate([
+    {
+	  role: 'fileMenu',
+	  label: 'App',
+	  submenu: [
+	    {
+          label: 'Quit',
+          accelerator: 'CmdOrCtrl+Q',
+          role: 'quit'
+        }
+      ]
+    },
+    {
+      role: 'editMenu',
+      label: 'Edit',
+      submenu: [
+        { role: 'undo' },
+        { role: 'redo' },
+        { type: 'separator' },
+        { role: 'cut' },
+        { role: 'copy' },
+        { role: 'paste' },
+        { role: 'pasteandmatchstyle' },
+        { role: 'delete' },
+        { type: 'separator' },
+        { role: 'selectall' }
+      ]
+    },
+    {
+      role: 'viewMenu'
+    },
+    {
+      role: 'help',
+      label: 'About',
+      submenu: [
+        { label: 'Electron Calculator v' + app.getVersion(), enabled: false },
+        { label: 'Created by Alex313031',
+          click() {
+            new BrowserWindow({width: 1024, height: 768}).loadURL('https://github.com/Alex313031/electron-calculator#readme');
+          }
+        },
+        {
+          type: 'separator'
+        },
+        {
+          label: 'View Humans.txt',
+          accelerator: 'CmdorCtrl+Alt+Shift+H',
+          click() {
+            const humansWindow = new BrowserWindow({width: isWin ? 350 : 350, height: isWin ? 400 : 400, title: "humans.txt"});
+            humansWindow.loadFile('./humans.txt');
+            electronLog.info('Opened humans.txt :)');
+          }
+        },
+        {
+          label: 'View License',
+          accelerator: 'CmdorCtrl+Alt+Shift+L',
+          click() {
+            const humansWindow = new BrowserWindow({width: isWin ? 532 : 532, height: isWin ? 642 : 624, title: "License"});
+            humansWindow.loadFile('./license.md');
+            electronLog.info('Opened license.md');
+          }
+        },
+        {
+          label: 'About App',
+          accelerator: 'CmdorCtrl+Alt+A',
+          click() {
+            const aboutWindow = new BrowserWindow({
+              width: isWin ? 350 : 350,
+              height: isWin ? 380 : 380,
+              title: "About App",
+              icon: process.platform === 'win32' ? path.join(__dirname, 'icon.ico') : path.join(__dirname, 'icon64.png'),
+              webPreferences: {
+                nodeIntegration: false,
+                nodeIntegrationInWorker: false,
+                contextIsolation: false,
+                sandbox: false,
+                experimentalFeatures: true,
+                webviewTag: true,
+                devTools: true,
+                javascript: true,
+                plugins: true,
+                enableRemoteModule: true,
+                preload: path.join(__dirname, 'preload.js'),
+              },
+            });
+            require("@electron/remote/main").enable(aboutWindow.webContents);
+            aboutWindow.loadFile('./about.html');
+            electronLog.info('Opened about.html');
+          }
+        }
+      ]
+    }
+    ]));
 
     // Load the index.html of the app.
     mainWindow.loadURL(url.format({
@@ -38,8 +144,6 @@ function createWindow () {
       protocol: 'file:',
       slashes: true
     }));
-    mainWindow.setMenuBarVisibility(true);
-    mainWindow.setResizable(true);
 }
 
 contextMenu({
@@ -57,14 +161,6 @@ contextMenu({
 });
 
 app.whenReady().then(createWindow);
-
-// Get app version from package.json
-var appVersion = app.getVersion();
-// Get Electron versions
-var electronVersion = process.versions.electron;
-var chromeVersion = process.versions.chrome;
-var nodeVersion = process.versions.node;
-var v8Version = process.versions.v8;
 
 electronLog.info('Welcome to Electron Calculator!');
 electronLog.info('App Version: ' + [ appVersion ]);
