fa79e740ac4c89adc459fd36dfcae5cd4f642647
Alexander Frick <Alex313031@gmail.com>
2023-09-06 12:23:59 -0500

    1.1.1

--- app/app.js
+++ app/app.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, Menu } = require('electron');
+const { app, BrowserWindow, Menu, nativeTheme } = require('electron');
 const electronLog = require('electron-log');
 const contextMenu = require('electron-context-menu');
 const path = require('path');
@@ -21,151 +21,181 @@ const isWin = process.platform === 'win32';
 const isMac = process.platform === 'darwin';
 
 function createWindow () {
-    let mainWindow = new BrowserWindow({
-      title: 'Electron Calculator',
-      resizable: true,
-      maximizable: true,
-      width: 350,
-      height: 450,
-      icon: process.platform === 'win32' ? path.join(__dirname, 'icon.ico') : path.join(__dirname, 'icon64.png'),
-      webPreferences: {
-        nodeIntegration: false,
-        nodeIntegrationInWorker: false,
-        contextIsolation: false,
-        sandbox: false,
-        experimentalFeatures: true,
-        webviewTag: true,
-        devTools: true,
-        javascript: true,
-        plugins: true,
-        enableRemoteModule: true,
-        // Preload before renderer processes
-        preload: path.join(__dirname, 'preload.js')
-      }
-    });
-    require("@electron/remote/main").enable(mainWindow.webContents);
-    Menu.setApplicationMenu(Menu.buildFromTemplate([
-    {
-	  role: 'fileMenu',
-	  label: 'App',
-	  submenu: [
-	    {
-          label: 'Relaunch',
-          click() {
-            app.relaunch();
-            app.quit();
-          }
-        },
-        { type: 'separator' },
-	    {
-          label: 'Quit',
-          accelerator: 'CmdOrCtrl+Q',
-          role: 'quit'
-        }
-      ]
-    },
-    {
-      role: 'editMenu',
-      label: 'Edit',
-      submenu: [
-        { role: 'undo' },
-        { role: 'redo' },
-        { type: 'separator' },
-        { role: 'cut' },
-        { role: 'copy' },
-        { role: 'paste' },
-        { role: 'pasteandmatchstyle' },
-        { role: 'delete' },
-        { type: 'separator' },
-        { role: 'selectall' }
-      ]
-    },
-    {
-      role: 'viewMenu'
-    },
-    {
-      role: 'help',
-      label: 'About',
-      submenu: [
-        { label: 'Electron Calculator v' + app.getVersion(), enabled: false },
-        { label: 'Created by Alex313031',
-          click() {
-            new BrowserWindow({width: 1024, height: 768}).loadURL('https://github.com/Alex313031/electron-calculator#readme');
-          }
-        },
-        {
-          type: 'separator'
-        },
-        {
-          label: 'View Humans.txt',
-          accelerator: 'CmdorCtrl+Alt+Shift+H',
-          click() {
-            const humansWindow = new BrowserWindow({width: isWin ? 350 : 350, height: isWin ? 400 : 400, title: "humans.txt"});
-            humansWindow.loadFile('./humans.txt');
-            electronLog.info('Opened humans.txt :)');
-          }
-        },
-        {
-          label: 'View License',
-          accelerator: 'CmdorCtrl+Alt+Shift+L',
-          click() {
-            const humansWindow = new BrowserWindow({width: isWin ? 532 : 532, height: isWin ? 642 : 624, title: "License"});
-            humansWindow.loadFile('./license.md');
-            electronLog.info('Opened license.md');
-          }
-        },
-        {
-          label: 'About App',
-          accelerator: 'CmdorCtrl+Alt+A',
-          click() {
-            const aboutWindow = new BrowserWindow({
-              width: isWin ? 350 : 350,
-              height: isWin ? 380 : 380,
-              title: "About App",
-              icon: process.platform === 'win32' ? path.join(__dirname, 'icon.ico') : path.join(__dirname, 'icon64.png'),
-              webPreferences: {
-                nodeIntegration: false,
-                nodeIntegrationInWorker: false,
-                contextIsolation: false,
-                sandbox: false,
-                experimentalFeatures: true,
-                webviewTag: true,
-                devTools: true,
-                javascript: true,
-                plugins: true,
-                enableRemoteModule: true,
-                preload: path.join(__dirname, 'preload.js'),
-              },
-            });
-            require("@electron/remote/main").enable(aboutWindow.webContents);
-            aboutWindow.loadFile('./about.html');
-            electronLog.info('Opened about.html');
-          }
-        }
-      ]
+  let mainWindow = new BrowserWindow({
+    title: 'Electron Calculator',
+    resizable: true,
+    maximizable: true,
+    width: 350,
+    height: 450,
+    icon: isWin ? path.join(__dirname, 'icon.ico') : path.join(__dirname, 'icon64.png'),
+    darkTheme: true,
+    webPreferences: {
+      nodeIntegration: false,
+      nodeIntegrationInWorker: false,
+      contextIsolation: false,
+      sandbox: false,
+      experimentalFeatures: true,
+      webviewTag: true,
+      devTools: true,
+      javascript: true,
+      plugins: true,
+      enableRemoteModule: true,
+      // Preload before renderer processes
+      preload: path.join(__dirname, 'preload.js')
     }
-    ]));
+  });
+  require("@electron/remote/main").enable(mainWindow.webContents);
+  Menu.setApplicationMenu(Menu.buildFromTemplate([
+  {
+    role: 'fileMenu',
+    label: 'App',
+    submenu: [
+      {
+        label: 'Relaunch',
+        click() {
+          app.relaunch();
+          app.quit();
+        }
+      },
+      { type: 'separator' },
+      {
+        label: 'Quit',
+        accelerator: 'CmdOrCtrl+Q',
+        role: 'quit'
+      }
+    ]
+  },
+  {
+    role: 'editMenu',
+    label: 'Edit',
+    submenu: [
+      { role: 'undo' },
+      { role: 'redo' },
+      { type: 'separator' },
+      { role: 'cut' },
+      { role: 'copy' },
+      { role: 'paste' },
+      { role: 'pasteandmatchstyle' },
+      { role: 'delete' },
+      { type: 'separator' },
+      { role: 'selectall' }
+    ]
+  },
+  {
+    role: 'viewMenu'
+  },
+  {
+    role: 'help',
+    label: 'About',
+    submenu: [
+      { label: 'Electron Calculator v' + app.getVersion(), enabled: false },
+      { label: 'Created by Alex313031',
+        click() {
+          new BrowserWindow({width: 1024, height: 768}).loadURL('https://github.com/Alex313031/electron-calculator#readme');
+        }
+      },
+      {
+        type: 'separator'
+      },
+      {
+        label: 'View Humans.txt',
+        accelerator: 'CmdorCtrl+Alt+Shift+H',
+        click() {
+          const humansWindow = new BrowserWindow({width: isWin ? 350 : 350, height: isWin ? 420 : 400, title: "humans.txt", darkTheme: true});
+          humansWindow.loadFile('./humans.txt');
+          electronLog.info('Opened humans.txt :)');
+        }
+      },
+      {
+        label: 'View License',
+        accelerator: 'CmdorCtrl+Alt+Shift+L',
+        click() {
+          const humansWindow = new BrowserWindow({width: isWin ? 532 : 532, height: isWin ? 642 : 624, title: "License", darkTheme: true});
+          humansWindow.loadFile('./license.md');
+          electronLog.info('Opened license.md');
+        }
+      },
+      {
+        label: 'About App',
+        accelerator: 'CmdorCtrl+Alt+A',
+        click() {
+          const aboutWindow = new BrowserWindow({
+            width: isWin ? 350 : 350,
+            height: isWin ? 400 : 380,
+            title: "About App",
+            icon: isWin ? path.join(__dirname, 'icon.ico') : path.join(__dirname, 'icon64.png'),
+            darkTheme: true,
+            webPreferences: {
+              nodeIntegration: false,
+              nodeIntegrationInWorker: false,
+              contextIsolation: false,
+              sandbox: false,
+              experimentalFeatures: true,
+              webviewTag: true,
+              devTools: true,
+              javascript: true,
+              plugins: true,
+              enableRemoteModule: true,
+              preload: path.join(__dirname, 'preload.js'),
+            },
+          });
+          require("@electron/remote/main").enable(aboutWindow.webContents);
+          aboutWindow.loadFile('./about.html');
+          electronLog.info('Opened about.html');
+        }
+      }
+    ]
+  }
+  ]));
 
-    // Load the index.html of the app.
-    mainWindow.loadURL(url.format({
-      pathname: path.join(__dirname, 'index.html'),
-      protocol: 'file:',
-      slashes: true
-    }));
+  nativeTheme.themeSource = 'dark';
+
+  // Load the index.html of the app.
+  mainWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'index.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
 }
 
 contextMenu({
-    showSelectAll: true,
-    showCopyImage: true,
-    showCopyImageAddress: true,
-    showSaveImageAs: true,
-    showCopyVideoAddress: true,
-    showSaveVideoAs: true,
-    showCopyLink: true,
-    showSaveLinkAs: true,
-    showInspectElement: true,
-    showLookUpSelection: true,
-    showSearchWithGoogle: true
+  showSelectAll: false,
+  showCopyImage: true,
+  showCopyImageAddress: true,
+  showSaveImageAs: true,
+  showCopyVideoAddress: true,
+  showSaveVideoAs: true,
+  showCopyLink: true,
+  showSaveLinkAs: true,
+  showInspectElement: true,
+  showLookUpSelection: true,
+  showSearchWithGoogle: true,
+  prepend: (defaultActions, parameters, browserWindow) => [
+  { label: 'Open Link in New Window',
+    // Only show it when right-clicking a link
+    visible: parameters.linkURL.trim().length > 0,
+    click: (linkURL) => {
+      const newWin = new BrowserWindow({
+        title: 'New Window',
+        width: 1024,
+        height: 768,
+        webPreferences: {
+          nodeIntegration: false,
+          nodeIntegrationInWorker: false,
+          contextIsolation: false,
+          sandbox: false,
+          experimentalFeatures: true,
+          webviewTag: true,
+          devTools: true,
+          javascript: true,
+          plugins: true,
+          enableRemoteModule: true,
+        }
+      });
+      const toURL = parameters.linkURL;
+      newWin.loadURL(toURL);
+    }
+  }]
 });
 
 app.whenReady().then(createWindow);
